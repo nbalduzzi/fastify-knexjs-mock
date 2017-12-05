0e88686054bb37f9f8f66783c7f228a9da7072eb
Nicolas Balduzzi <nbalduzzi@zemsania.com>
2017-12-05 14:04:23 +0100

    test

--- test.js
+++ test.js
@@ -2,13 +2,14 @@ const fastify = require('fastify')()
 const tap = require('tap')
 const fastifyKnexJSMock = require('./index')
 
-tap.test('fastify.tracker should exist', test => {
-  test.plan(2)
+tap.test('fastify.knex and fastify.tracker should exist', test => {
+  test.plan(3)
 
   fastify.register(fastifyKnexJSMock, { client: 'mysql' })
 
   fastify.ready(err => {
     test.error(err)
+    test.ok(fastify.knex)
     test.ok(fastify.tracker)
 
     fastify.close(() => test.end())
